1a174ae85a55ef4f6ae6a3ab217415eecd7fd1cf
Dimitris Bozelos <dbozelos@gmail.com>
2021-05-05 02:26:49 +0000

    Added factory for generating clients

---
+++ src/DirectApi/ClientFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace KrystalCode\SagePayments\Sdk\DirectApi;
+
+use KrystalCode\SagePayments\Sdk\DirectApi\Resource\ApiHealth;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Factory for generating Direct API resource clients.
+ */
+class ClientFactory
+{
+    /**
+     * The logger to pass to the client.
+     *
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected LoggerInterface $logger;
+
+    /**
+     * Constructs a new ClientFactory object.
+     *
+     * @param \Psr\Log\LoggerInterface $logger
+     *     The logger to pass to the client.
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Returns an initialized client for the requested resource.
+     *
+     * @param string $api
+     *     The ID of the API to prepare the client for.
+     *
+     * @return \KrystalCode\SagePayments\Sdk\DirectApi\ClientInterface
+     *     The initialized API client.
+     *
+     * @throws \InvalidArgumentException
+     *     When an unknown API ID is given.
+     */
+    public function get(string $api, array $config): ClientInterface
+    {
+        switch ($api) {
+            case ApiHealth::ID:
+                return new ApiHealth($this->logger, $config);
+
+            default:
+                throw new \InvalidArgumentException(sprintf(
+                    'Unknown API "%s"',
+                    $api
+                ));
+        }
+    }
+}
