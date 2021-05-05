3c1c7a5f44c13f27611e53dc982cd5f708833a76
Dimitris Bozelos <dbozelos@gmail.com>
2021-05-05 02:19:12 +0000

    Added API Health resource client implementation

---
+++ src/DirectApi/Resource/ApiHealth.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace KrystalCode\SagePayments\Sdk\DirectApi\Resource;
+
+use KrystalCode\SagePayments\Sdk\DirectApi\ClientBase;
+
+/**
+ * The API Health resource.
+ */
+class ApiHealth extends ClientBase
+{
+    /**
+     * The ID of the API Health API.
+     */
+    public const ID = 'api_health';
+
+    /**
+     * Returns basic information about the front-end API.
+     *
+     * @return object|null
+     *     The response as an \stdClass object, or null if the response could
+     *     not be decoded.
+     *
+     * @link https://developer.sagepayments.com/bankcard-ecommerce-moto/apis/get/ping
+     */
+    public function getPing(): object
+    {
+        return $this->getRequest('ping');
+    }
+
+    /**
+     * Returns basic information about the front-end and back-end APIs.
+     *
+     * @return object|null
+     *     The response as an \stdClass object, or null if the response could
+     *     not be decoded.
+     *
+     * @link https://developer.sagepayments.com/bankcard-ecommerce-moto/apis/get/status
+     */
+    public function getStatus(): object
+    {
+        return $this->getRequest('status');
+    }
+}
